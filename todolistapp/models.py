640bcca871972b512ea838aeb93dfdd8a4ae5067
Assed Mus <assedkac47@gmail.com>
2024-09-29 23:05:08 +0100

    feat(models): users
    adding user models
    
    BREAKING CHANGE:

--- todolistapp/models.py
+++ todolistapp/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
-class List(models.Model):
+class Task(models.Model):
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)         
     date_creation = models.DateTimeField(auto_now_add=True)  
     date_modification = models.DateTimeField(null=True, blank=True)
     termine = models.BooleanField(default=False)
+
+class Users (models.Model):
+    phone_number=models.CharField(max_length=20)
+    user= models.OneToOneField(User ,on_delete=models.CASCADE ,null=True, blank=True)
 
 
     
--- todolistapp/views.py
+++ todolistapp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import render 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,14 +5,11 @@ from rest_framework.status import *
 from rest_framework import viewsets, status
 from .models import *
 from .serializers import *
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from django.contrib.auth.models import Group
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from .models import Client, User  
-from .serializers import ClientSerializer, UserSerializer
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate,logout
-
-# Create your views here.
+""" api_view()
+def afficher_taches(request):
+    taches = Task.objects.all() 
+    return Response({'taches': taches},status=200)  
+ """
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()  
+    serializer_class = TaskSerializer
